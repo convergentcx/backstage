acd13839866b06d484c3048b73feb193e184940f
Logan Saether <lsaether@protonmail.com>
2019-01-19 19:59:30 +0100

    logan 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import styled from 'styled-components';
-import web3 from 'web3';
+import Web3 from 'web3';
+
+import {
+  Church,
+  Flowers,
+  Man,
+  Sunflower,
+} from './img';
+
+const stdWeb3 = () => {
+  const w3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io'));
+  return w3;
+}
+
+const sign = async data => {
+  const sig = await Web3.eth.sign(data, Web3.eth.defaultAccount);
+  return sig;
+}
+
+const recoverAddress = (data, sig, givenAddr) => {
+  const w3 = stdWeb3();
+  return w3.eth.accounts.recover(data, sig);
+}
 
 const AccessButton = styled.button`
   pointer: cursor;
@@ -13,7 +35,7 @@ const AccessButton = styled.button`
   transition: 0.3s;
   width: 300px;
   height: 200px;
-  font-size: 32px;
+  font-size: 1.8rem;
   :hover {
     color: #FFF;
     background: #0044FF;
@@ -29,7 +51,7 @@ class AccessControlled extends Component {
     const { activated } = this.props;
 
     return (
-      <div>
+      <div style={{ width: '100%', height: '100%' }}>
         {
           activated &&
           this.props.children
@@ -42,21 +64,96 @@ class AccessControlled extends Component {
 class App extends Component {
   state = {
     activated: false,
+    account: null,
+    global: {
+      ethereum: null,
+      web3: null,
+    },
+    signature: {
+      data: null,
+      account: null,
+    },
   }
 
   activate = () => {
     this.setState({
       activated: !this.state.activated,
     });
+    this.props.onSignIn('some data');
+  }
+
+  componentDidMount() {
+    this.setState({
+      global: window,
+    });
+  }
+
+  sign = async () => {
+    const { account, global } = this.state;
+    const data = Web3.utils.toHex('Hello World');
+    const res = await global.web3.eth.personal.sign(data, account)
+    if (account === recoverAddress(data, res)) {
+      this.props.onSignIn();
+    }
+  }
+  
+  turnOnWeb3 = async () => {
+    const { global } = this.state;
+    if (typeof global === 'object') {
+      if (global.ethereum) {
+        global.web3 = new Web3(global.ethereum);
+        try {
+          // Request account access if needed
+          await global.ethereum.enable();
+          const accounts = await global.web3.eth.getAccounts();
+          this.setState({
+            account: accounts[0],
+          });
+          // Acccounts now exposed
+          console.log('ENABLED');
+        } catch (error) {
+          // User denied account access...
+          console.log('USER DENIED');
+        }
+      }
+      // Legacy dapp browsers...
+      else if (global.web3) {
+        global.web3 = new Web3(global.web3.currentProvider);
+        // Acccounts always exposed
+        console.log('ENABLED')
+      }
+      // Non-dapp browsers...
+      else {
+        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+      }
+    }
   }
 
   render() {
     return (
-      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-        <AccessButton onClick={this.activate}>Access</AccessButton>
-        <AccessControlled tokens={5} activated={this.state.activated}>
-          <div style={{ color: '#000' }}>Hello World</div>
-        </AccessControlled>
+      <div style={{ display: 'flex', flexDirection: 'row', padding: '', justifyContent: '', alignItems: '', height: '100vh' }}>
+        <div style={{ display: 'flex', width: '40%', background: '#2424D0', minHeight: '100%' }}>
+
+        </div>
+        <div style={{ display: 'flex', width: '60%', background: '#810b0b', minHeight: '100%', flexDirection: 'column', alignItems: 'center' }}>
+          <h1 style={{ color: 'silver' }}>Exclusive Zone</h1>
+          { this.props.authorized ?
+            <AccessControlled tokens={5} activated={this.props.authorized}>
+              <div style={{ background: '#810b0b', display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '100%', alignItems: 'center', height: '100%', marginTop: '120px' }}>
+                <img src={Church} style={{ width: '300px', height: '200px', paddingBottom: '16px' }} alt="img"/>
+                <img src={Man} style={{ width: '300px', height: '200px', paddingBottom: '16px' }} alt="img"/>
+                <img src={Flowers} style={{ width: '300px', height: '200px', paddingBottom: '16px' }} alt="img"/>
+                <img src={Sunflower} style={{ width: '300px', height: '200px', paddingBottom: '16px' }} alt="img"/>
+              </div>
+            </AccessControlled>
+            :
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <AccessButton onClick={this.turnOnWeb3}>Access<br/>With Convergent</AccessButton>
+            <button onClick={this.sign}>Sign</button>
+            <button onClick={this.deploy}>Test Deploy</button>
+            </div>
+          }
+          </div>
       </div>
     );
   }
@@ -64,8 +161,8 @@ class App extends Component {
 
 const mapStateToProps = state => {
   return {
-    authorized: state.auth
-  }
+    authorized: state.auth,
+  };
 }
 
 const mapDispatchToProps = dispatch => {
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,14 +1,34 @@
+import web3 from 'web3';
 
-const initialState = {auth: false};
-
+const initialState = {
+	address: null,
+	auth: false,
+	web3: null,
+};
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'SIGN') {
-        return {
-            auth: !state.auth
-        }
-    }
-    return state
+	if (action.type === 'SIGN') {
+		console.log('Redux::sign')
+		return {...state, auth: !state.auth};
+	}
+	// if (action.type === 'WEB3') {
+	// 	console.log('Redux::web3')
+	// 	let provider;
+	// 	if (typeof window.ethereum !== 'undefined'
+	// 		|| (typeof window.web3 !== 'undefined')) {
+	// 		// Already has web3!
+	// 		console.log('Backstage Web3 Enabled');
+	// 		// Web3 browser user detected. You can now use the provider.
+	// 		provider = window['ethereum'] || window.web3.currentProvider
+	// 	} else {
+			
+	// 	}
+
+	// 	// const w3 = 
+	// 	return {...state, web3: w3}
+	// }
+
+	return state;
 }
 
 export default reducer;
