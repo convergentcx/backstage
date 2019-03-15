736db2f8445cf12ea8d61597fbd8126cacc41b0c
acrdlph <achill@convergent.cx>
2019-03-15 14:53:05 +0100

    changed port number back

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,6 @@ app.use(contentRoutes);
 
 mongoose.connect('mongodb+srv://achill:w8BG6xR351pqX6DC@cluster0-xfiey.mongodb.net/tokens')
 .then(_ => {
-  app.listen(80);
+  app.listen(3002);
 })
 .catch(console.error);
